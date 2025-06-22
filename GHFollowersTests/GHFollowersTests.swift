9de9d689a378bc9d20e43dd2acc66fc2fb3469b1
Chirag Shetty <chiragranjit.shetty@team.telstra.com>
2025-06-22 20:32:09 +0530

    Initial Commit

---
+++ GHFollowersTests/GHFollowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  GHFollowersTests.swift
+//  GHFollowersTests
+//
+//  Created by Shetty, Chirag Ranjit on 22/06/25.
+//
+
+import Testing
+@testable import GHFollowers
+
+struct GHFollowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
